10e3ef4c020d4fe511323b4c24a36809cb3ea775
Amish Verma <amishverma1998@gmail.com>
2022-06-04 11:51:22 +0530

    Using state to create counter app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div>
-      <h1>Hello World!!!!</h1>
+    <div className="App">
+      <header>
+        <h1>Counter App using State</h1>
+      </header>
+      <h2>Current value of count is {count} </h2>
+      <button>Reset Counter</button>
+      <button>Increase Counter</button>
+      <button>Decrease Counter</button>
     </div>
   );
 }
